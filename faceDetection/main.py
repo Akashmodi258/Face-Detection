069a43f9b72dfed54018cf388eaa489ef69ad7f0
Akashmodi258 <136723836+Akashmodi258@users.noreply.github.com>
2024-04-02 23:04:42 +0530

    Add files via upload

---
+++ faceDetection/main.py
@@ -0,0 +1,15 @@
+import time
+import cv2 
+import mediapipe as mp
+print("heloowporld")
+cap=cv2.VideoCapture(0)
+ptime=0
+while True:
+    success, img=cap.read()
+    cTime = time.time()
+    
+    fps=1/(cTime-ptime)
+    ptime=cTime
+    cv2.putText(img,f'fps : {int(fps)}',(20,70),cv2.FONT_HERSHEY_PLAIN,3,(0,255,0),2)
+    cv2.imshow("image",img)
+    cv2.waitKey(1)
